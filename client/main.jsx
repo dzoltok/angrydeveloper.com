b0529e75f13f3f0b77d0f979e9f046613e8093a4
David Zoltok <dzoltok@thestar.ca>
2015-12-18 15:55:50 -0500

    Adding beginnings of React and Webpack functionality

---
+++ client/main.jsx
@@ -0,0 +1,9 @@
+require('./assets/app.css');
+require('./assets/menu.css');
+require('./assets/blabs.css');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/layout/App.jsx');
+
+ReactDOM.render(<App/>, document.getElementById('app'));
