ccdcd33becddcc6ed86b8c6b6a0de9f32d9e3893
Dave Zoltok <zoltok@gmail.com>
2015-12-27 17:22:51 -0500

    Modifying library grouping

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     loaders: [
       { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel', query: { presets: [ 'react', 'es2015' ] } },
       { test: /\.css$/, loader: 'style!css' },
-      { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.scss$/, loader: 'style!css!sass' }
     ]
   },
